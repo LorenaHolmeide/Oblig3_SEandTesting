83d4835e1590f2176ff951148ef4ee298b40e477
LorenaHolmeide <lorenah@hiof.no>
2022-10-18 22:35:01 +0200

    test

--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -2,33 +2,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LeapYearTest {
-    //Test to check if LeapYear
+    //Test to check if LeapYear.
     @Test
     public void TheYear_DividedBy4AndNot100_IsLeapYear(){
         assertTrue(LeapYear.isLeapYear(2024));
     }
-    //Test to check if LeapYear
+    //Test to check if LeapYear.
     @Test
     public void TheYear_DividedBy400_IsLeapYear(){
         assertTrue(LeapYear.isLeapYear(2000));
     }
-    //Test to check if LeapYear
+    //Test to check if LeapYear.
     @Test
     public void TheYear_DividedBy400ButNot4000_IsLeapYear(){
         assertTrue(LeapYear.isLeapYear(6000));
     }
 
-    //Test to check if not LeapYear
+    //Test to check if not LeapYear.
     @Test
     public void TheYear_NotDividingBy4_NotLeapYear(){
     assertFalse(LeapYear.isLeapYear(1881));
     }
-    //Test to check if not LeapYear
+    //Test to check if not LeapYear.
     @Test
     public void TheYear_DividedBy100ButNot400_NotLeapYear(){
         assertFalse(LeapYear.isLeapYear(1700));
     }
-    //Test to check if not LeapYear
+    //Test to check if not LeapYear.
     @Test
     public void TheYear_DividedBy4000_NotLeapYear(){
         assertFalse(LeapYear.isLeapYear(8000));
